f67f918ddf6d89c00663392ef91b84f461235615
Stef Lewandowski <stef@stef.io>
2023-10-31 12:23:07 +0000

    Init

---
+++ pages/api/homework.ts
@@ -0,0 +1,88 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const {
+    keyStage,
+    subject,
+    numberOfTasks = 1,
+    actionPlan,
+    summary,
+    objectives,
+    transcript,
+    title,
+  } = req.body;
+  const completion = await openai.createCompletion({
+    model: "gpt-3.5-turbo-16k",
+    prompt: reviewPrompt({
+      keyStage,
+      subject,
+      numberOfTasks,
+      actionPlan,
+      summary,
+      objectives,
+      transcript,
+      title,
+    }),
+    //max_tokens: 150,
+    temperature: 0.8,
+    top_p: 1.0,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function reviewPrompt({
+  keyStage,
+  subject,
+  numberOfTasks,
+  actionPlan,
+  summary,
+  objectives,
+  transcript,
+  title,
+}) {
+  return `Context:
+
+  You are a differentiated homework task generating bot. You will be provided with information about a ${keyStage} ${subject} lesson that a teacher in a UK school has delivered to their class.
+  
+  Task:
+  
+  Your task is to generate a set of potential homework tasks based on the provided lesson that could be given to students based on their particular educational needs.
+  
+  Intended output:
+  
+  You should provide a valid markdown document containing a set of homework tasks that the student could do to continue their learning on the topics and learning objectives covered in the lesson.
+  
+  Write the suggested tasks as if you are talking to the student and explaining what they need to do. Provide all the information that they need to be able to complete the homework. It is okay to refer to the contents of the slides because they should have access to them.
+  
+  Adapt your homework task to the needs of the student based on the action plan. 
+  Do not include any unrelated content. Do not mention the name of the teacher. Do not refer to content that is not covered in the lesson.
+  
+  Instruct the student to pick ${numberOfTasks} of the tasks that they would find the most interesting.
+  
+  Action plan for the student:
+  
+  ${actionPlan}
+  
+  Your response should be based on best practice for how to support children with autism in their learning.
+  
+  Lesson title: ${title}
+
+  Lesson summary:
+  
+  ${summary}
+  
+  Learning objectives:
+  
+  ${objectives}
+  
+  Lesson transcript:
+  
+  ${transcript}`;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,225 @@
-import { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import { useState, useRef, useEffect } from "react";
+import hljs from "highlight.js";
 import React from "react";
 
-const HomePage: NextPage = () => {
-  return (
-    <>
-    <Head>
-      <title>OpenAI API Starter</title>
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        Astro <span className="text-[hsl(280,100%,70%)]">AI</span>
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="/review"
-          >
-            <h3 className="text-2xl font-bold">Product Reviews →</h3>
-            <div className="text-lg">
-              Generate purpose-built product reviews.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="/short-story"
-      
-          >
-            <h3 className="text-2xl font-bold">Short Story →</h3>
-            <div className="text-lg">
-Create a 2-sentence horror story.
-            </div>
-          </Link>
-        </div>
-       
-      </div>
-    </main>
-  </>
-  );
-};
+export default function Review() {
+  // Create a ref for the div element
+  const textDivRef = useRef<HTMLDivElement>(null);
+  const [keyStageInput, setKeyStageInput] = useState("");
+  const [subjectInput, setSubjectInput] = useState("");
+  const [numberOfTasksInput, setNumberOfTasksInput] = useState("");
+  const [actionPlanInput, setActionPlanInput] = useState("");
+  const [summaryInput, setSummaryInput] = useState("");
+  const [objectivesInput, setObjectivesInput] = useState("");
+  const [transcriptInput, setTranscriptInput] = useState("");
+  const [titleInput, setTitleInput] = useState("");
+  const [result, setResult] = useState(() => "");
+  const [isLoading, setIsLoading] = useState(false);
 
-export default HomePage;
+  // Add a click event listener to the copy icon that copies the text in the div to the clipboard when clicked
+  useEffect(() => {
+    const copyIcon = document.querySelector(".copy-icon");
+    if (!copyIcon) return;
+    copyIcon.addEventListener("click", () => {
+      const textDiv = textDivRef.current;
+      let text;
+      if (textDivRef.current) {
+        text = textDivRef.current.textContent;
+      }
+      // Create a hidden textarea element
+      const textArea = document.createElement("textarea");
+      textArea.value = text;
+      document.body.appendChild(textArea);
+
+      // Select the text in the textarea
+      textArea.select();
+
+      // Copy the text to the clipboard
+      document.execCommand("copy");
+
+      // Remove the textarea element
+      document.body.removeChild(textArea);
+    });
+  }, []); // Run this only once
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    const response = await fetch("/api/short-story", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        keyStage: keyStageInput,
+        subject: subjectInput,
+        numberOfTasks: numberOfTasksInput,
+        actionPlan: actionPlanInput,
+        summary: summaryInput,
+        objectives: objectivesInput,
+        transcript: transcriptInput,
+        title: titleInput,
+      }),
+    });
+    const data = await response.json();
+    console.log("data", data);
+    console.log("data.result", data.result);
+
+    let rawResult = data.result;
+
+    console.log("rawResult");
+
+    // set result to the highlighted code. Address this error: Argument of type 'string' is not assignable to parameter of type '(prevState: undefined) => undefined'.ts(2345)
+    setResult(rawResult);
+
+    setKeyStageInput("");
+    setSubjectInput("");
+    setNumberOfTasksInput("");
+    setActionPlanInput("");
+    setSummaryInput("");
+    setObjectivesInput("");
+    setTranscriptInput("");
+    setTitleInput("");
+    setIsLoading(false);
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Differentiated Homework Generator</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main
+        className="flex flex-col 
+                    items-center justify-center m-20"
+      >
+        <h3 className="text-slate-900 text-xl mb-3">
+          Differentiated Homework Generator
+        </h3>
+        <p className="text-slate-700 text-lg mb-3">
+          Adapt a homework task to the needs of a particular student
+        </p>
+        <form onSubmit={onSubmit}>
+          <input
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="title"
+            placeholder="Enter the title of the lesson"
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.target.value)}
+          />
+
+          <input
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="keyStage"
+            placeholder="Key Stage 3"
+            value={keyStageInput}
+            onChange={(e) => setKeyStageInput(e.target.value)}
+          />
+
+          <textarea
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="keyStage"
+            placeholder="A summary of the lesson"
+            value={summaryInput}
+            onChange={(e) => setSummaryInput(e.target.value)}
+          />
+
+          <textarea
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="objectives"
+            placeholder="Your learning objectives"
+            value={keyStageInput}
+            onChange={(e) => setObjectivesInput(e.target.value)}
+          />
+
+          {
+            (keyStage,
+            subject,
+            (numberOfTasks = 1),
+            actionPlan,
+            summary,
+            objectives,
+            transcript)
+          }
+
+          <textarea
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="actionPlan"
+            placeholder="The student's action plan or needs. Eg. reading age"
+            value={actionPlanInput}
+            onChange={(e) => setActionPlanInput(e.target.value)}
+          />
+
+          <textarea
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="transcript"
+            placeholder="A transcript of the lesson, or any free text that you have that explains what the lesson covered"
+            value={transcriptInput}
+            onChange={(e) => setTranscriptInput(e.target.value)}
+          />
+
+          <button
+            className="text-sm w-full bg-fuchsia-600 h-7 text-white
+                              rounded-2xl mb-10"
+            type="submit"
+          >
+            Generate homework
+          </button>
+        </form>
+        {isLoading ? (
+          <p>Loading... be patient.. may take 30s+</p>
+        ) : result ? (
+          <div className="relative w-2/4 ">
+            <div className="rounded-md border-spacing-2 border-slate-900 bg-slate-100 break-words max-w-500 overflow-x-auto  ">
+              <div
+                ref={textDivRef}
+                className="m-5 "
+                dangerouslySetInnerHTML={{ __html: result }}
+              />
+            </div>
+            <div className="copy-icon absolute top-0 right-0 mt-2 mr-2 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-copy"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="8" y="8" width="12" height="12" rx="2"></rect>
+                <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+              </svg>
+            </div>
+          </div>
+        ) : null}
+      </main>
+    </div>
+  );
+}
