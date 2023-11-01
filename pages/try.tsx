505d25cbef8779af9312497dd810ebefd08d28d8
Stef Lewandowski <stef@stef.io>
2023-11-01 12:21:19 +0000

    Make try dynamic

--- pages/try.tsx
+++ pages/try.tsx
@@ -4,11 +4,8 @@ const DynamicTry = dynamic(() => import("../components/Try"), {
   loading: () => <p>Loading...</p>,
 });
 
-
-const TryPage = () {
-  return (
-    <DynamicTry />
-  )
-}
+const TryPage = () => {
+  return <DynamicTry />;
+};
 
 export default TryPage;
